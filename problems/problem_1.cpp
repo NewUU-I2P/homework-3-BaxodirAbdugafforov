b6e2a11085ba55f0b12b1e6e4c18f3e9802b7d27
baxodirabdugafforov <baxodirabdugafforov2408@gmail.com>
2023-11-13 22:45:30 +0500

    solved

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,21 +1,22 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-
+    double f1 = 0.4;
+    double f2 = 0.12;
+    double f3 = 1.4;
+    double f4 = 1.5;
     if (consumed_water <= 30) {
-        cost =  13 + consumed_water * 0.4;
+        cost =  13 + (consumed_water * f1);
     }
     else if (consumed_water > 30 and consumed_water <= 50 ){
-        cost = 13 + ((consumed_water - 30) * 0.12) + (30 * 0.4);
+        cost = 13 + ((consumed_water - 30) * f2) + (30 * f1);
     }
     else if (consumed_water > 30 and consumed_water <= 60){
-        cost = 13 + ((consumed_water - 50) * 1.4) + (30 * 0.4) + (20 * 0.12);
+        cost = 13 + ((consumed_water - 50) * f3) + (30 * f1) + (20 * f2);
     }
-
     else if (consumed_water > 60) {
-        cost = 13 + ((consumed_water - 60) * 1.5) * (30 * 0.4) + (20 * 0.12) + (10 * 1.4);
+        cost = 13 + ((consumed_water - 60) * f4) * (30 * f1) + (20 * f2) + (10 * f3);
+        return;
     }
-    return;
-
 
 }
