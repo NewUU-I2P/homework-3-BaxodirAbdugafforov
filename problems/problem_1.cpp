3b662f355a0c8c106b17ae67e32d71cf23dabf53
baxodirabdugafforov <baxodirabdugafforov2408@gmail.com>
2023-11-13 22:29:49 +0500

    solved

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,5 +2,18 @@ void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
 
+    if (consumed_water <= 30) {
+       cost =  13 + consumed_water * 0.4;
+    }
+    else if (consumed_water > 30 and consumed_water <= 50 ){
+        cost = 13 + ((consumed_water - 30) * 0.12) + (30 * 0.4);
+    }
+    else if (consumed_water > 30 and consumed_water <= 60){
+        cost = 13 + ((consumed_water - 50) * 1.4) + (30 * 0.4) + (20 * 0.12);
+    }
+
+    else if (consumed_water > 60) {
+        cost = 13 + ((consumed_water - 60) * 1.5) * (30 * 0.4) + (20 * 0.12) + (10 * 1.4);
+    }
     return cost;
 }
